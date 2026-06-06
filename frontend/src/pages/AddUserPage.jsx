fe027bad570e9969e0f1aeae870095a13fb35176
Aditya Deshmukh <adityadeshmukh911@gmail.com>
2026-06-06 18:29:51 +0530

    updated API URL to Render and CORS for production

--- frontend/src/pages/AddUserPage.jsx
+++ frontend/src/pages/AddUserPage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = import.meta.env.VITE_API_URL;;
 
 function AddUserPage() {
   const [form, setForm] = useState({ name: "", email: "", city: "" });
